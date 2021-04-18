5d758c5cbee6e9aa4594c6e44bc1cf74865f4f11
Alejandro <alesf22@gmail.com>
2021-04-18 13:18:47 -0600

    upgrade

---
+++ subpages/works/works_script.js
@@ -0,0 +1,23 @@
+function setNewImage1 (){
+    document.getElementById("img").src="../../assets/Preview/songsP.png";
+}
+
+function setNewImage2 (){
+    document.getElementById("img").src="../../assets/Preview/typesP.png";
+}
+
+function setNewImage3 (){
+    document.getElementById("img").src="../../assets/Preview/OPP.png";
+}
+
+function setNewImage4 (){
+    document.getElementById("img").src="../../assets/Preview/navbarP.png";
+}
+
+function setNewImage5 (){
+    document.getElementById("img").src="../../assets/Preview/proofP.png";
+}
+
+function setOldImage (){
+    document.getElementById("img").src="../../assets/Preview/base.png"; 
+}
